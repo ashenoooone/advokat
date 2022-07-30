b5b900dd91b5f5fc6c352c67a8b1943ea76927cf
Igor Koneshov <igorkoneshov@mail.ru>
2022-07-31 00:49:22 +0400

    feat: add contact & fix font in legal & blog key

---
+++ src/components/ContactPopup/ContactPopup.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import './ContactPopup.scss';
+import whatsup from '../../assets/whatsup.svg';
+
+const ContactPopup = ({ isOpen, setOpen }) => {
+  const handleEscapeClose = (e) => e.key == 'Escape' && setOpen(false);
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscapeClose);
+    return () => {
+      document.removeEventListener('keydown', handleEscapeClose);
+    };
+  }, [isOpen]);
+
+  return (
+    <div className={`contact ${isOpen && 'contact_active'}`}>
+      <div className='contact__container'>
+        <div className='contact__numbers'>
+          <p className='contact__number'>+7 919 827 51 57</p>
+          <img src={whatsup} alt='whatsup icon' className='contact__whatsup' />
+        </div>
+        <p className='contact__mail'>romario.yakovenko@yandex.ru</p>
+        <p className='contact__adress'>г. Саратов, ул. Сакко и Ванцетти, 55</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPopup;
