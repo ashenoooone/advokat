b7294bc95528956b698a047f5e48340e8da453c2
Igor Koneshov <igorkoneshov@mail.ru>
2022-08-01 16:31:40 +0400

    feat: add animate to contact popup

--- src/components/ContactPopup/ContactPopup.js
+++ src/components/ContactPopup/ContactPopup.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import './ContactPopup.scss';
 import whatsup from '../../assets/whatsup.svg';
 
@@ -18,16 +19,32 @@ const ContactPopup = ({ isOpen, setOpen }) => {
   }, [isOpen]);
 
   return (
-    <div className={`contact ${isOpen && 'contact_active'}`}>
-      <div id='container' className='contact__container'>
-        <div className='contact__numbers'>
-          <p className='contact__number'>+7 919 827 51 57</p>
-          <img src={whatsup} alt='whatsup icon' className='contact__whatsup' />
-        </div>
-        <p className='contact__mail'>romario.yakovenko@yandex.ru</p>
-        <p className='contact__adress'>г. Саратов, ул. Сакко и Ванцетти, 55</p>
-      </div>
-    </div>
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div
+          initial={{ height: 0, y: -100 }}
+          animate={{ height: 'auto', y: 0 }}
+          transition={{ duration: 0.5 }}
+          exit={{ height: 0, y: -100 }}
+          className={`contact ${isOpen && 'contact_active'}`}
+        >
+          <div id='container' className='contact__container'>
+            <div className='contact__numbers'>
+              <p className='contact__number'>+7 919 827 51 57</p>
+              <img
+                src={whatsup}
+                alt='whatsup icon'
+                className='contact__whatsup'
+              />
+            </div>
+            <p className='contact__mail'>romario.yakovenko@yandex.ru</p>
+            <p className='contact__adress'>
+              г. Саратов, ул. Сакко и Ванцетти, 55
+            </p>
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
