b4acb00ed2b2705b3d8b344c0efb61125231f41d
Igor Koneshov <igorkoneshov@mail.ru>
2022-08-01 20:36:22 +0400

    feat: add adaptive animation header menu and popup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,14 @@ import logo from '../../assets/Vector.svg';
 import burger from '../../assets/burger.svg';
 import ContactPopup from '../ContactPopup/ContactPopup';
 import { AnimatePresence, motion } from 'framer-motion';
-
+import { useMatchMedia } from '../../hook/useMatchMedia';
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
   const [isHide, setHide] = useState(false);
   const [pos, setPos] = useState(100);
-  const [isBurger, setBurger] = useState(false);
+  const [isBurger, setBurger] = useState(true);
   const handleOpenClick = () => setOpen(!isOpen);
-
+  const isBurgerActive = useMatchMedia('(max-width: 1023px)', true);
   useEffect(() => {
     const scrollHandler = () => {
       if (window.scrollY >= 100 && window.scrollY > pos) {
@@ -27,10 +27,12 @@ const Header = () => {
       window.removeEventListener('scroll', scrollHandler);
     };
   }, [pos]);
+  useEffect(() => {
+    if (!isBurgerActive) setBurger(true);
+  }, [isBurgerActive]);
   return (
     <header className={`header ${isHide && 'header_hide'}`}>
       <div className='content header__content'>
-        <ContactPopup isOpen={isOpen} setOpen={setOpen} />
         <div className='header__info'>
           <img src={logo} alt='logo' className='header__logo' />
           <h2 className='header__name'>
@@ -39,49 +41,85 @@ const Header = () => {
             роман яковенко
           </h2>
         </div>
-        <nav className='header__nav'>
-          <a href='#reviews' className='header__link'>
-            Отзывы
-          </a>
-          <a href='#blog' className='header__link'>
-            Блог
-          </a>
-          <span className='header__link' onClick={handleOpenClick}>
-            Контакты
-          </span>
+        <AnimatePresence>
+          {!isBurgerActive ? (
+            <nav
+              className={`${!isBurger ? 'header__nav-burger' : 'header__nav'}`}
+            >
+              <a
+                href='#reviews'
+                className={`${!isBurger ? 'header__nav-link' : 'header__link'}`}
+              >
+                Отзывы
+              </a>
+              <a
+                href='#blog'
+                className={`${!isBurger ? 'header__nav-link' : 'header__link'}`}
+              >
+                Блог
+              </a>
+              <span
+                className={`${!isBurger ? 'header__nav-link' : 'header__link'}`}
+                onClick={handleOpenClick}
+              >
+                Контакты
+              </span>
+              <ContactPopup isOpen={isOpen} setOpen={setOpen} />
+            </nav>
+          ) : (
+            !isBurger && (
+              <motion.nav
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{
+                  duration: 0.5,
+                }}
+                exit={{ opacity: 0 }}
+                className={`${
+                  !isBurger ? 'header__nav-burger' : 'header__nav'
+                }`}
+              >
+                <div className='header__navs'>
+                  <a
+                    href='#reviews'
+                    className={`${
+                      !isBurger ? 'header__nav-link' : 'header__link'
+                    }`}
+                  >
+                    Отзывы
+                  </a>
+                  <a
+                    href='#blog'
+                    className={`${
+                      !isBurger ? 'header__nav-link' : 'header__link'
+                    }`}
+                  >
+                    Блог
+                  </a>
+                  <span
+                    className={`${
+                      !isBurger ? 'header__nav-link' : 'header__link'
+                    }`}
+                    onClick={handleOpenClick}
+                  >
+                    Контакты
+                  </span>
+                  <ContactPopup isOpen={isOpen} setOpen={setOpen} />
+                </div>
+              </motion.nav>
+            )
+          )}
+          {/* )} */}
+        </AnimatePresence>
+        {isBurgerActive && (
           <img
             src={burger}
             alt='burger menu'
             className={'header__burger'}
             onClick={() => setBurger(!isBurger)}
           />
-        </nav>
-      </div>
-      <AnimatePresence>
-        {isBurger && (
-          <motion.nav
-            initial={{ height: 0 }}
-            animate={{ height: 'auto' }}
-            transition={{
-              duration: 0.5,
-            }}
-            exit={{ height: 0 }}
-            className='header__nav-burger'
-          >
-            <div className='header__navs'>
-              <a href='#reviews' className='header__nav-link'>
-                Отзывы
-              </a>
-              <a href='#blog' className='header__nav-link'>
-                Блог
-              </a>
-              <span className='header__nav-link' onClick={handleOpenClick}>
-                Контакты
-              </span>
-            </div>
-          </motion.nav>
         )}
-      </AnimatePresence>
+      </div>
     </header>
   );
 };
