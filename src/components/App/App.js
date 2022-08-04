f9eced5ed996ba17c8357f2441c2504c35d6a3b3
Igor Koneshov <igorkoneshov@mail.ru>
2022-08-05 00:45:12 +0400

    feat: add react-router-dom frame

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,31 @@ import Blog from '../Blog/Blog';
 import Consul from '../Consul/Consul';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import { Routes, Route } from 'react-router-dom';
 import LegalAssistance from '../LegalAssistance/LegalAssistance';
 import ReviewsContainer from '../ReviewsContainer/ReviewsContainer';
 
 function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Consul />
-      <LegalAssistance />
-      <ReviewsContainer />
-      <Blog />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<>
+							<Consul />
+							<LegalAssistance />
+							<ReviewsContainer />
+							<Blog />
+						</>
+					}
+				/>
+				<Route path='/article/:id' element='' />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Article/Article.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Article.scss';
+
+const Article = () => {
+	return <main className='main'></main>;
+};
+
+export default Article;
