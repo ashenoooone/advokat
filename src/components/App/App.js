388415882dc2a87d80be3522ad98602ea56f8109
Roman Gontar <ashenoooone@gmail.com>
2022-08-05 18:32:17 +0400

    feat: 404 page ready

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,28 +6,30 @@ import Header from '../Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import LegalAssistance from '../LegalAssistance/LegalAssistance';
 import ReviewsContainer from '../ReviewsContainer/ReviewsContainer';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 
 function App() {
-	return (
-		<div className='App'>
-			<Header />
-			<Routes>
-				<Route
-					path='/'
-					element={
-						<>
-							<Consul />
-							<LegalAssistance />
-							<ReviewsContainer />
-							<Blog />
-						</>
-					}
-				/>
-				<Route path='/article/:id' element='' />
-			</Routes>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div className='page'>
+              <Consul />
+              <LegalAssistance />
+              <ReviewsContainer />
+              <Blog />
+            </div>
+          }
+        />
+        <Route path='/article/:id' element='' />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
