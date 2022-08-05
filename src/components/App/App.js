1f3255878e102d6974ede6cd18002a0c4ae16bc8
Roman Gontar <ashenoooone@gmail.com>
2022-08-05 19:42:16 +0400

    feat: 404 page and article page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,8 @@ import Header from '../Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import LegalAssistance from '../LegalAssistance/LegalAssistance';
 import ReviewsContainer from '../ReviewsContainer/ReviewsContainer';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import Article from '../Article/Article';
 
 function App() {
   return (
@@ -23,7 +25,8 @@ function App() {
             </>
           }
         />
-        <Route path='/article/:id' element='' />
+        <Route path='/*' element={<NotFoundPage />} />
+        <Route path='/article/:id' element={<Article />} />
       </Routes>
       <Footer />
     </div>
