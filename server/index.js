92a7799fa3a5533c5ceeb6ef84a63a54bd567456
Igor Koneshov <igorkoneshov@mail.ru>
2022-09-11 23:53:22 +0400

    feat: add file uploading

--- server/index.js
+++ server/index.js
@@ -5,12 +5,19 @@ const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static('uploads'));
 app.use(cors());
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 app.use(express.json());
 app.use('/api', router);
 // замыкающий т.к. на нем прекращается работа. из-за невызванного next
