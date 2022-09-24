0dddb6e09f9c0a78191d5412a06212bd12fbab24
Roman Gontar <ashenoooone@gmail.com>
2022-09-24 15:49:52 +0400

    feat: reaction popup

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import './Article.scss';
-import ReactionPopup from '../ReactionPopup/ReactionPopup';
-import like from '../../assets/like.svg';
-import dislike from '../../assets/dislike.svg';
-import BlogCard from '../BlogCard/BlogCard';
-import Comment from '../Comment/Comment';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import axios from 'axios';
-import { useCallback } from 'react';
+import React, { useState } from "react";
+import "./Article.scss";
+import ReactionPopup from "../ReactionPopup/ReactionPopup";
+import like from "../../assets/like.svg";
+import dislike from "../../assets/dislike.svg";
+import BlogCard from "../BlogCard/BlogCard";
+import Comment from "../Comment/Comment";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+import { useCallback } from "react";
 
 const Article = () => {
   const { id } = useParams();
   const [isOpen, setOpen] = useState(false);
-  const [name, setName] = useState('');
-  const [comment, setComment] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [comment, setComment] = useState("");
+  const [email, setEmail] = useState("");
   const [blogData, setBlogData] = useState({});
   const [comments, setComments] = useState([]);
   const [anotherBlog, setAnotherBlog] = useState([]);
@@ -23,22 +23,27 @@ const Article = () => {
   const handleTogglePopup = () => {
     setOpen(!isOpen);
   };
-  const handleNameChange = e => setName(e.target.value);
-  const handleCommentChange = e => setComment(e.target.value);
-  const handleEmailChange = e => setEmail(e.target.value);
-  const handleFormSubmit = async e => {
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleCommentChange = (e) => setComment(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    const response = await axios.post(`http://134.0.115.164:7000/api/article`, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
+    const response = await axios.post(
+      `http://134.0.115.164:7000/api/article`,
+      {
+        name,
+        text: comment,
+        email,
+        blogId: id,
       },
-      name,
-      text: comment,
-      email,
-      blogId: id,
-    });
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
     if (response) {
-      setComments(state => [
+      setComments((state) => [
         ...state,
         {
           id: response.data.id,
@@ -48,9 +53,9 @@ const Article = () => {
           data: response.data.data,
         },
       ]);
-      setComment('');
-      setEmail('');
-      setName('');
+      setComment("");
+      setEmail("");
+      setName("");
     }
   };
   /* api handler */
@@ -59,7 +64,7 @@ const Article = () => {
       `http://134.0.115.164:7000/api/article/${id}`,
       {
         headers: {
-          'Access-Control-Allow-Origin': '*',
+          "Access-Control-Allow-Origin": "*",
         },
       }
     );
@@ -71,7 +76,7 @@ const Article = () => {
         `http://134.0.115.164:7000/api/comments/${id}`,
         {
           headers: {
-            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Origin": "*",
           },
         }
       );
@@ -83,7 +88,7 @@ const Article = () => {
       `http://134.0.115.164:7000/api/another-blog`,
       {
         headers: {
-          'Access-Control-Allow-Origin': '*',
+          "Access-Control-Allow-Origin": "*",
         },
       }
     );
@@ -96,83 +101,83 @@ const Article = () => {
     getAnotherBlog();
   }, [getInfo, getComments, getAnotherBlog]);
   return (
-    <main className='main' key={id}>
-      <div className='main__container'>
-        <img src={blogData?.image} alt='main img' className='main__img' />
-        <p className='data'>{blogData?.time}</p>
-        <h1 className='main__title'>{blogData?.title}</h1>
-        <div className='text-container'>
-          <pre className='text'>{blogData?.text}</pre>
+    <main className="main" key={id}>
+      <div className="main__container">
+        <img src={blogData?.image} alt="main img" className="main__img" />
+        <p className="data">{blogData?.time}</p>
+        <h1 className="main__title">{blogData?.title}</h1>
+        <div className="text-container">
+          <pre className="text">{blogData?.text}</pre>
         </div>
         <div
           className={`card__buttos-container post-reaction`}
           onClick={handleTogglePopup}
         >
           <div className={`card__button-box`}>
-            <img src={like} alt='like' className='card__button' />
-            <span className='card__number'>
+            <img src={like} alt="like" className="card__button" />
+            <span className="card__number">
               {blogData.likes ? blogData.likes.length : 0}
             </span>
           </div>
           <div className={`card__button-box`}>
-            <img src={dislike} alt='dislike' className='card__button' />
-            <span className='card__number'>
+            <img src={dislike} alt="dislike" className="card__button" />
+            <span className="card__number">
               {blogData.dislikes ? blogData.dislikes.length : 0}
             </span>
           </div>
           <ReactionPopup isOpen={isOpen} setOpen={setOpen} />
         </div>
-        <div className='another-blog'>
-          <h2 className='another-blog__title'>Другие статьи</h2>
-          <div className='another-blog__container'>
-            {anotherBlog.map(blog => (
-              <BlogCard key={'blog/' + blog.id} {...blog} />
+        <div className="another-blog">
+          <h2 className="another-blog__title">Другие статьи</h2>
+          <div className="another-blog__container">
+            {anotherBlog.map((blog) => (
+              <BlogCard key={"blog/" + blog.id} {...blog} />
             ))}
           </div>
         </div>
-        <div className='rating'>
-          <h2 className='rating__title'>Комментарии</h2>
-          <div className='rating__comments'>
+        <div className="rating">
+          <h2 className="rating__title">Комментарии</h2>
+          <div className="rating__comments">
             {comments &&
-              comments.map(com => <Comment key={'com/' + com.id} {...com} />)}
+              comments.map((com) => <Comment key={"com/" + com.id} {...com} />)}
           </div>
         </div>
       </div>
-      <div className='reviews-form'>
-        <div className='reviews-form__container'>
-          <h3 className='review-popup__title'>Оставить отзыв</h3>
-          <form className='popup-form' onSubmit={handleFormSubmit}>
-            <div className='review-popup__inputs'>
+      <div className="reviews-form">
+        <div className="reviews-form__container">
+          <h3 className="review-popup__title">Оставить отзыв</h3>
+          <form className="popup-form" onSubmit={handleFormSubmit}>
+            <div className="review-popup__inputs">
               <input
-                type='text'
-                className='popup-form__input'
-                placeholder='Имя'
+                type="text"
+                className="popup-form__input"
+                placeholder="Имя"
                 value={name}
                 required
                 onChange={handleNameChange}
               />
               <input
-                type='text'
-                className='popup-form__input'
-                placeholder='Email'
+                type="text"
+                className="popup-form__input"
+                placeholder="Email"
                 value={email}
                 required
                 onChange={handleEmailChange}
               />
             </div>
             <input
-              type='text'
-              className='popup-form__input'
-              placeholder='Комментарий'
+              type="text"
+              className="popup-form__input"
+              placeholder="Комментарий"
               value={comment}
               required
               onChange={handleCommentChange}
             />
-            <button className='reviews-form__button'>Отправить</button>
+            <button className="reviews-form__button">Отправить</button>
           </form>
-          <p className='review-popup__conf'>
-            Нажимая на кнопку, я соглашаюсь с условиями{' '}
-            <a href='#' className='popup__link'>
+          <p className="review-popup__conf">
+            Нажимая на кнопку, я соглашаюсь с условиями{" "}
+            <a href="#" className="popup__link">
               политики конфиденциальности.
             </a>
           </p>
--- client/src/components/ReactionPopup/ReactionPopup.js
+++ client/src/components/ReactionPopup/ReactionPopup.js
@@ -1,13 +1,13 @@
-import React, { useCallback, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import './ReactionPopup.scss';
-import ConfPopup from '../ConfPopup/ConfPopup';
-import axios from 'axios';
-import ReCAPTCHA from 'react-google-recaptcha';
+import React, { useCallback, useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./ReactionPopup.scss";
+import ConfPopup from "../ConfPopup/ConfPopup";
+import axios from "axios";
+import ReCAPTCHA from "react-google-recaptcha";
 
-const ReactionPopup = ({ isOpen, isLike, id, setCards, onClose }) => {
+const ReactionPopup = ({ isLike, id, setCards, onClose }) => {
   const [isPopupOpened, setIsPopupOpened] = useState(false);
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [isCaptchaVisible, setIsCaptchaVisible] = useState(false);
   let siteKey = process.env.REACT_APP_SITE_KEY;
 
@@ -15,122 +15,141 @@ const ReactionPopup = ({ isOpen, isLike, id, setCards, onClose }) => {
     const response = await axios.post(
       `http://134.0.115.164:7000/api/review-card/${id}`,
       {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
         like: email,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
       }
     );
     response &&
-      setCards(state => {
-        const updateCard = state.find(card => card.id === id);
+      setCards((state) => {
+        const updateCard = state.find((card) => card.id === id);
         updateCard.likes.push(email);
-        const cards = state.filter(card => card.id !== id);
+        const cards = state.filter((card) => card.id !== id);
         return [...cards, updateCard].sort((a, b) => a.id - b.id);
       });
   }, [email, id]);
+
   const sendDislikeReaction = useCallback(async () => {
     const response = await axios.post(
       `http://134.0.115.164:7000/api/review-card/${id}`,
       {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
         dislike: email,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
       }
     );
     response &&
-      setCards(state => {
-        const updateCard = state.find(card => card.id === id);
+      setCards((state) => {
+        const updateCard = state.find((card) => card.id === id);
         updateCard.dislikes.push(email);
-        const cards = state.filter(card => card.id !== id);
+        const cards = state.filter((card) => card.id !== id);
         return [...cards, updateCard].sort((a, b) => a.id - b.id);
       });
   }, [email, id]);
-  const onCloseConfPopupClick = e => {
+
+  const onCloseConfPopupClick = (e) => {
     e.preventDefault();
     const classes = e.target.classList;
     e.stopPropagation();
-    if (classes.contains('popup') || classes.contains('popup__close-button'))
+    if (classes.contains("popup") || classes.contains("popup__close-button"))
       setIsPopupOpened(false);
   };
-  const onOpenPopupClick = e => {
+
+  const onOpenPopupClick = (e) => {
     e.preventDefault();
     setIsPopupOpened(true);
   };
-  const handleSubmitReactionForm = e => {
+
+  const handleSubmitReactionForm = (e) => {
     e.preventDefault();
     setIsCaptchaVisible(true);
   };
-  const handleEmailChange = e => setEmail(e.target.value);
-  const onOuterPopupClick = e => {
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+
+  const onOuterPopupClick = (e) => {
+    console.log(e.target.closest("div"));
     if (
-      e.target.closest('div').className !== 'reaction-popup__container' &&
-      e.target.closest('div').className !== 'submit-button-container'
+      e.target.closest("div").className !== "reaction-popup__container" &&
+      e.target.closest("div").className !== "submit-button-container"
     ) {
       onClose();
     }
   };
-  const onCaptchaChange = async e => {
+  const onCaptchaChange = async (e) => {
     setIsCaptchaVisible(false);
     if (isLike) {
       await sendLikeReaction();
     } else {
       await sendDislikeReaction();
     }
-    setEmail('');
+    setEmail("");
     onClose();
   };
+
+  useEffect(() => {
+    window.addEventListener("click", onOuterPopupClick);
+    return () => {
+      window.removeEventListener("click", onOuterPopupClick);
+    };
+  }, []);
+
   return (
     <AnimatePresence>
-      {isOpen && (
-        <motion.div
-          initial={{ height: 0 }}
-          animate={{ height: 'auto' }}
-          exit={{ height: 0 }}
-          transition={{ duration: 0.5 }}
-          className='reaction-popup'
-          onClick={onOuterPopupClick}
-        >
-          <div className='reaction-popup__container'>
-            <h3 className='consul-popup__title'>Поставить оценку</h3>
-            <form className='popup-form' onSubmit={handleSubmitReactionForm}>
-              <input
-                type='email'
-                className='popup-form__input'
-                placeholder='Email'
-                value={email}
-                onChange={handleEmailChange}
+      (
+      <motion.div
+        initial={{ height: 0 }}
+        animate={{ height: "auto" }}
+        exit={{ height: 0 }}
+        transition={{ duration: 0.5 }}
+        className="reaction-popup"
+        // onClick={onOuterPopupClick}
+      >
+        <div className="reaction-popup__container">
+          <h3 className="consul-popup__title">Поставить оценку</h3>
+          <form className="popup-form" onSubmit={handleSubmitReactionForm}>
+            <input
+              type="email"
+              className="popup-form__input"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+            <div className="submit-button-container">
+              <ReCAPTCHA
+                sitekey={process.env.REACT_APP_SITE_KEY}
+                className={`captcha ${
+                  isCaptchaVisible ? "captcha_active" : ""
+                }`}
+                onChange={onCaptchaChange}
               />
-              <div className='submit-button-container'>
-                <ReCAPTCHA
-                  sitekey={process.env.REACT_APP_SITE_KEY}
-                  className={`captcha ${
-                    isCaptchaVisible ? 'captcha_active' : ''
-                  }`}
-                  onChange={onCaptchaChange}
-                />
-                <button
-                  className='button button_default'
-                  style={{ width: '100%' }}
-                >
-                  Поставить
-                </button>
-              </div>
-            </form>
-            <p className='consul-popup__conf'>
-              Нажимая на кнопку, я соглашаюсь с условиями{' '}
-              <a
-                href='#'
-                className='reaction-popup__link'
-                onClick={onOpenPopupClick}
+              <button
+                className="button button_default"
+                style={{ width: "100%" }}
               >
-                политики конфиденциальности.
-              </a>
-            </p>
-          </div>
-        </motion.div>
-      )}
+                Поставить
+              </button>
+            </div>
+          </form>
+          <p className="consul-popup__conf">
+            Нажимая на кнопку, я соглашаюсь с условиями{" "}
+            <a
+              href="#"
+              className="reaction-popup__link"
+              onClick={onOpenPopupClick}
+            >
+              политики конфиденциальности.
+            </a>
+          </p>
+        </div>
+      </motion.div>
       <ConfPopup
         key={2}
         onClosePopupClick={onCloseConfPopupClick}
