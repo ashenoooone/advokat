8bea88993434faf8faf76eb02a7455fe9ba94479
Roman Gontar <ashenoooone@gmail.com>
2022-09-30 00:08:56 +0400

    fix: deploy fixes

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Blog from '../Blog/Blog';
-import Consul from '../Consul/Consul';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import { Routes, Route } from 'react-router-dom';
-import LegalAssistance from '../LegalAssistance/LegalAssistance';
-import ReviewsContainer from '../ReviewsContainer/ReviewsContainer';
-import NotFoundPage from '../NotFoundPage/NotFoundPage';
-import Article from '../Article/Article';
+import React from "react";
+import Blog from "../Blog/Blog";
+import Consul from "../Consul/Consul";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import { Routes, Route } from "react-router-dom";
+import LegalAssistance from "../LegalAssistance/LegalAssistance";
+import ReviewsContainer from "../ReviewsContainer/ReviewsContainer";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
+import Article from "../Article/Article";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <>
               <Consul />
@@ -25,8 +25,8 @@ function App() {
             </>
           }
         />
-        <Route path='/*' element={<NotFoundPage />} />
-        <Route path='/article/:id' element={<Article />} />
+        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="/article/:id" element={<Article />} />
       </Routes>
       <Footer />
     </div>
